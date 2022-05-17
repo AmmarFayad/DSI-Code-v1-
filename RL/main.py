9b6001df03e3a374185d22779bc46242aa5ee9ad
Ammar Fayad <ammarfayad@C02FK46AMD6M.tld>
2022-05-17 16:42:46 -0400

    Added the RL

--- RL/main.py
+++ RL/main.py
@@ -24,6 +24,9 @@ parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
 parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
                     help='Temperature parameter α determines the relative importance of the entropy\
                             term against the reward (default: 0.2)')
+parser.add_argument('--lamda_dsi', type=float, default=0.2, metavar='G',
+                    help='Temperature parameter α determines the relative importance of the DSI\
+                            term against the reward (default: 0.2)')
 parser.add_argument('--automatic_entropy_tuning', type=bool, default=False, metavar='G',
                     help='Automaically adjust α (default: False)')
 parser.add_argument('--seed', type=int, default=123456, metavar='N',
@@ -44,6 +47,7 @@ parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
                     help='size of replay buffer (default: 10000000)')
 parser.add_argument('--cuda', action="store_true",
                     help='run on CUDA (default: False)')
+
 args = parser.parse_args()
 
 # Environment
